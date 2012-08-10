8ab921ab75c525f846c1e5c34ca3d02200cf8757
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-08-10 01:50:59 -0700

    Put up a screen, and quit on Esc.

--- Main.hs
+++ Main.hs
@@ -1,10 +1,30 @@
 module Main where
 
-import Graphics.UI.SDL
+import Control.Monad
+import Data.IORef
 
-mainLoop :: IO ()
-mainLoop = return ()
+import Graphics.UI.SDL as SDL
+import Graphics.UI.SDL.Image
+
+setScreen :: IO Surface
+setScreen = setVideoMode 640 480 32 [DoubleBuf, SWSurface]
+
+mainLoop :: Surface -> IORef Bool -> IO ()
+mainLoop screen quit = do
+    event <- pollEvent
+    case event of
+        NoEvent -> return ()
+        KeyDown (Keysym SDLK_ESCAPE _ _) -> writeIORef quit True
+        _ -> putStrLn $ show event
+    SDL.flip screen
+    doQuit <- readIORef quit
+    unless doQuit $ mainLoop screen quit
+
+actualMain :: IO ()
+actualMain = do
+    screen <- setScreen
+    quit <- newIORef False
+    mainLoop screen quit
 
 main :: IO ()
-main = do
-    withInit [InitEverything] mainLoop
+main = withInit [InitEverything] actualMain
