dec3607a50314f167c71d3939c63daaed13b59b6
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-08-13 02:22:31 -0700

    Get Heather on the screen.

--- Main.hs
+++ Main.hs
@@ -5,15 +5,15 @@ import Control.Lens
 import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
-import Data.IORef
 import qualified Data.Map as Map
+import Data.Map.Lens
 
 import Graphics.UI.SDL as SDL
 import Graphics.UI.SDL.Image
 
 data SpriteSheet = SpriteSheet { _ssFilePath :: FilePath
                                , _ssAtlas :: Surface
-                               , _ssdw, _ssdh, _ssw, _ssh :: Int }
+                               , _ssw, _ssh, _ssdw, _ssdh :: Int }
     deriving (Show)
 
 makeLenses ''SpriteSheet
@@ -41,16 +41,23 @@ getScreen = do
     return $ GlobalData screen Map.empty False
 
 coordsAt :: Int -> Int -> Int -> Int -> Int -> (Int, Int)
-coordsAt x y w h i = let
-    w' = x `div` w
-    (y', x') = i `divMod` w'
-    in (x', y')
+coordsAt w h dw dh i = let
+    w' = w `div` dw
+    (y, x) = i `divMod` w'
+    in (x * dw, y * dh)
 
 toSprite :: Int -> Getter SpriteSheet Sprite
 toSprite i = let
-    f (SpriteSheet _ atlas w h x y) = let
-        (x', y') = coordsAt x y w h i
-        in Sprite atlas $ Rect x' y' (x' + x) (y' + y)
+    f (SpriteSheet _ atlas w h dw dh) = let
+        (x', y') = coordsAt w h dw dh i
+        in Sprite atlas $ Rect x' y' (x' + dw) (y' + dh)
+    in to f
+
+spriteAt :: String -> Int -> Getter ImageMap Sprite
+spriteAt s i = let
+    f m = case m ^. at s of
+        Just sheet -> sheet ^. toSprite i
+        Nothing -> undefined
     in to f
 
 loadImage :: FilePath -> ImageMap -> IO ImageMap
@@ -59,7 +66,7 @@ loadImage path m = do
     rect <- getClipRect surface
     let w = rectW rect
         h = rectH rect
-        sheet = SpriteSheet path surface 408 325 w h
+        sheet = SpriteSheet path surface w h 408 325
     return $ Map.insert path sheet m
 
 loadSheet :: FilePath -> Loop
@@ -71,8 +78,19 @@ loadSheet path = do
 clearScreen :: Loop
 clearScreen = do
     s <- use screen
-    _ <- lift $ fillRect s Nothing (Pixel 0x0)
+    _ <- lift $ fillRect s Nothing (Pixel 0x333333)
     return ()
+
+blitSprite :: Sprite -> Loop
+blitSprite sprite = let
+    atlas = sprite ^. sAtlas
+    sr = sprite ^. rect
+    in do
+        s <- use screen
+        lift . putStrLn $ show sprite
+        _ <- lift $ blitSurface atlas (Just sr) s (Just sr)
+        lift $ updateRect s sr
+        return ()
 
 finishFrame :: Loop
 finishFrame = do
@@ -88,6 +106,8 @@ mainLoop = loadImages >> loop
             NoEvent -> return ()
             KeyDown (Keysym SDLK_ESCAPE _ _) -> quitFlag .= True
             _ -> lift . putStrLn $ show event
+        sprite <- use $ images . spriteAt "heather1.png" 0
+        blitSprite sprite
         clearScreen
         finishFrame
         q <- use quitFlag
