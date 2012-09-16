e558b675273efff2f8470d8b443d7fa8b0db83e8
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-09-16 11:48:46 -0700

    Transform: Go with something simpler.
    From a thought experiment over the past few days.

--- Working/Transform.hs
+++ Working/Transform.hs
@@ -1,38 +1,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Working.Transform where
 
-import Control.Lens
-
 import Working.Box
 
-data Transform a = Transform { _tFactorX, _tFactorY :: a
-                             , _tOffsetX, _tOffsetY :: a }
+data Transform a = Scale a a
+                 | Translate a a
     deriving (Show)
 
-makeLenses ''Transform
-
--- Define a transformation from one box to another.
-transformRef :: (Fractional a, Ord a) => GoodBox a -> GoodBox a -> Transform a
-transformRef src dest = let
-    origin b = bX .~ 0 $ bY .~ 0 $ b
-    usrc = src ^. bTag
-    udest = dest ^. bTag
-    -- Grab the offsets. bBottom and bLeft would work here too.
-    ox = udest ^. bX - usrc ^. bX
-    oy = udest ^. bY - usrc ^. bY
-    -- Move the boxes so that their bottom-left corners are at the origin.
-    -- This'll make it easy to calculate the factors.
-    src' = origin usrc
-    dest' = origin udest
-    fx = dest' ^. bRight / src' ^. bRight
-    fy = dest' ^. bTop / src' ^. bTop
-    in Transform fx fy ox oy
-
 -- Apply a transformation to a box, yielding a transformed box.
-transform :: (Fractional a, Ord a) => Transform a -> GoodBox a -> GoodBox a
-transform (Transform fx fy ox oy) b = let
-    -- First, the translation...
-    trans b = bTag . bX +~ ox $ bTag . bY +~ oy $ b
-    -- ...then, the scaling.
-    scale b = bTag . bW *~ fx $ bTag . bH *~ fy $ b
-    in scale . trans $ b
+transform :: Transform a -> GoodBox a -> GoodBox a
+transform (Scale sx sy) b = bTag . bX *~ sx $ bTag . bY *~ sy $ b
+transform (Translate tx ty) b = bTag . bX +~ sx $ bTag . bY +~ sy $ b
