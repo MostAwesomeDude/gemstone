a2bbf7988fba90a5ff50031377c1673bbdf651ed
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-01-08 07:49:11 -0800

    Rewrite Box.

--- Gemstone/Box.hs
+++ Gemstone/Box.hs
@@ -1,11 +1,8 @@
-{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE EmptyDataDecls, TemplateHaskell #-}
 module Gemstone.Box (
-    Box(),
-    GoodBox, BadBox,
+    Box(), BoxLike(..), unBox,
     pInter, bInter,
-    makeBadBox, makeBox, validateBox,
     makeXYWH,
-    bTag,
     bLeft, bBottom, bRight, bTop,
     bW, bH, bW', bH',
     bX, bY, bXY,
@@ -13,90 +10,91 @@ module Gemstone.Box (
 ) where
 
 import Control.Lens
-import Data.Tagged
-import Graphics.Rendering.OpenGL
 
-data Box v = Box (Vertex2 v) (Vertex2 v)
+data BoxLike v = BoxLike { _bx1, _by1, _bx2, _by2 :: v }
     deriving (Show)
 
-data Good
-data Bad
+makeLenses ''BoxLike
 
-type GoodBox v = Tagged Good (Box v)
-type BadBox v = Tagged Bad (Box v)
+newtype Box v = Box { unBox :: BoxLike v }
+    deriving (Show)
 
--- Whether two vertices would determine the lower-left and upper-right corners
--- of a rectangle.
-pInter :: Ord v => Vertex2 v -> Vertex2 v -> Bool
-pInter (Vertex2 x1 y1) (Vertex2 x2 y2) = x1 < x2 && y1 < y2
+-- | Whether two vertices would determine the lower-left and upper-right corners
+--   of a rectangle.
+--
+--   This is the predicate for determining whether a box is valid.
+pInter :: Ord v => v -> v -> v -> v -> Bool
+pInter x1 y1 x2 y2 = x1 < x2 && y1 < y2
 
--- Whether two boxes intersect.
--- The boxes have to be good.
-bInter :: Ord v => GoodBox v -> GoodBox v -> Bool
-bInter (Tagged (Box v1 v2)) (Tagged (Box v3 v4)) =
-    pInter v1 v4 && pInter v3 v2
+-- | Prism for getting a Box from a BoxLike.
+--
+--   The Box constructor isn't exported, so this is the only way to obtain a
+--   valid Box.
+box :: Ord v => Simple Prism (BoxLike v) (Box v)
+box = prism unBox f
+    where
+    f b | pred b = Right $ Box b
+        | otherwise = Left b
+    pred (BoxLike x1 y1 x2 y2) = pInter x1 y1 x2 y2
 
--- Make a possibly valid box.
-makeBadBox :: Vertex2 v -> Vertex2 v -> BadBox v
-makeBadBox v1 v2 = Tagged $ Box v1 v2
+-- | Whether two boxes intersect.
+--
+--   The boxes have to be good.
+bInter :: Ord v => Box v -> Box v -> Bool
+bInter (Box b1) (Box b2) =
+    pInter (b1 ^. bx1) (b1 ^. by1) (b2 ^. bx2) (b2 ^. by2) &&
+    pInter (b2 ^. bx1) (b2 ^. by1) (b1 ^. bx2) (b1 ^. by2)
 
--- Validate a box. This will slap you in the face at runtime if you didn't
--- make a good box.
-validateBox :: (Ord v, Show v) => BadBox v -> GoodBox v
-validateBox b@(Tagged (Box v1 v2))
-    | pInter v1 v2 = Tagged $ Box v1 v2
-    | otherwise    = error $ "validateBox: Bad box " ++ show b
+-- | Make a box with width and height.
+makeXYWH :: (Ord v, Num v) => v -> v -> v -> v -> BoxLike v
+makeXYWH x y w h = BoxLike x y (x + w) (y + h)
 
--- Possibly make a valid box.
-makeBox :: (Ord v, Show v) => Vertex2 v -> Vertex2 v -> GoodBox v
-makeBox v1 v2 = validateBox $ makeBadBox v1 v2
+-- | Like makeXYWH, but valid.
+--
+--   Includes a slap on the face if the box is not valid.
+makeXYWHValid :: (Ord v, Num v) => v -> v -> v -> v -> Box v
+makeXYWHValid x y w h = case makeXYWH x y w h ^? box of
+    Just b -> b
+    Nothing -> error "makeXYWHValid: Zero width or height"
 
--- Make a valid box with width and height.
--- This function will slap you hard if you specify zero width or height.
-makeXYWH :: (Ord v, Num v) => v -> v -> v -> v -> GoodBox v
-makeXYWH x y w h
-    | w > 0 && h > 0 = Tagged $ Box (Vertex2 x y) (Vertex2 (x + w) (y + h))
-    | otherwise      = error "makeXYWH: Zero width or height"
+-- | Resize a box by moving an edge.
+--
+--   Inherently unsafe.
+bLeft, bBottom, bRight, bTop :: Simple Lens (BoxLike a) a
+bLeft   = bx1
+bBottom = by1
+bRight  = bx2
+bTop    = by2
 
--- Lens to unwrap and rewrap a good box with its tag. Revalidates the box on
--- setting.
-bTag :: (Ord a, Show a) => Simple Lens (GoodBox a) (Box a)
-bTag f (Tagged b) = fmap (\b' -> validateBox . Tagged $ b') (f b)
+-- | Resize a box by changing its width or height.
+--
+--   Inherently unsafe.
+bW, bH, bW', bH' :: Num a => Simple Lens (BoxLike a) a
+bW  f (BoxLike x1 y1 x2 y2) =
+    fmap (\w -> BoxLike x1 y1 (x2 + w) y2) (f (x2 - x1))
+bH  f (BoxLike x1 y1 x2 y2) =
+    fmap (\h -> BoxLike x1 y1 x2 (y2 + h)) (f (y2 - y1))
+bW' f (BoxLike x1 y1 x2 y2) =
+    fmap (\w -> BoxLike (x1 - w) y1 x2 y2) (f (x2 - x1))
+bH' f (BoxLike x1 y1 x2 y2) =
+    fmap (\h -> BoxLike x1 (y1 - h) x2 y2) (f (y2 - y1))
 
--- Resize a box by moving an edge.
-bLeft, bBottom, bRight, bTop :: Simple Lens (Box a) a
-bLeft f (Box (Vertex2 x y) v) = fmap (\x' -> Box (Vertex2 x' y) v) (f x)
-bBottom f (Box (Vertex2 x y) v) = fmap (\y' -> Box (Vertex2 x y') v) (f y)
-bRight f (Box v (Vertex2 x y)) = fmap (\x' -> Box v (Vertex2 x' y)) (f x)
-bTop f (Box v (Vertex2 x y)) = fmap (Box v . Vertex2 x)           (f y)
-
--- Resize a box by changing its width or height.
-bW, bH, bW', bH' :: Num a => Simple Lens (Box a) a
-bW f (Box (Vertex2 x y) (Vertex2 x' y')) =
-    fmap (\w -> Box (Vertex2 x y) (Vertex2 (x + w) y')) (f (x' - x))
-bH f (Box (Vertex2 x y) (Vertex2 x' y')) =
-    fmap (\h -> Box (Vertex2 x y) (Vertex2 x' (y + h))) (f (y' - y))
-bW' f (Box (Vertex2 x y) (Vertex2 x' y')) =
-    fmap (\w -> Box (Vertex2 (x' - w) y) (Vertex2 x' y')) (f (x' - x))
-bH' f (Box (Vertex2 x y) (Vertex2 x' y')) =
-    fmap (\h -> Box (Vertex2 x (y' - h)) (Vertex2 x' y)) (f (y' - y))
-
--- Move a box.
+-- | Move a box.
 bX, bY :: Num a => Simple Lens (Box a) a
-bX f (Box (Vertex2 x y) (Vertex2 x' y')) =
-    fmap (\w -> Box (Vertex2 w y) (Vertex2 (w + x' - x) y')) (f x)
-bY f (Box (Vertex2 x y) (Vertex2 x' y')) =
-    fmap (\h -> Box (Vertex2 x h) (Vertex2 x' (h + y' - y))) (f y)
+bX f (Box (BoxLike x1 y1 x2 y2)) =
+    fmap (\x' -> Box (BoxLike x' y1 (x' + x2 - x1) y2)) (f x1)
+bY f (Box (BoxLike x1 y1 x2 y2)) =
+    fmap (\y' -> Box (BoxLike x1 y1 x2 (y' + y2 - y1))) (f y1)
 
 -- Move a box more efficiently.
 bXY :: Num a => Simple Lens (Box a) (a, a)
-bXY f (Box (Vertex2 x y) (Vertex2 x' y')) = let
-    f' (w, h) = Box (Vertex2 w h) (Vertex2 (w + x' - x) (h + y' - y))
-    in fmap f' (f (x, y))
+bXY f (Box (BoxLike x1 y1 x2 y2)) = let
+    f' (w, h) = Box $ BoxLike w h (w + x2 - x1) (h + y2 - y1)
+    in fmap f' $ f (x1, y1)
 
 -- Scale a box.
-scaleBox :: (Eq v, Num v) => v -> v -> GoodBox v -> GoodBox v
+scaleBox :: (Eq v, Num v) => v -> v -> Box v -> Box v
 scaleBox 0 _ _ = error "scaleBox: Zero width"
 scaleBox _ 0 _ = error "scaleBox: Zero height"
-scaleBox sx sy (Tagged (Box (Vertex2 x1 y1) (Vertex2 x2 y2))) =
-    Tagged $ Box (Vertex2 (x1 * sx) (y1 * sy)) (Vertex2 (x2 * sx) (y2 * sy))
+scaleBox sx sy (Box (BoxLike x1 y1 x2 y2)) =
+    Box $ BoxLike (x1 * sx) (y1 * sy) (x2 * sx) (y2 * sy)
