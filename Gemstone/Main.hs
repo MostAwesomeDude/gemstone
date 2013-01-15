ef9d5d08b7e381c17f61a58bf5baa4e67535f2c9
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-01-14 22:07:32 -0800

    Split out main loop setup.

---
+++ Gemstone/Main.hs
@@ -0,0 +1,14 @@
+module Gemstone.Main where
+
+import Control.Monad
+import Control.Monad.Trans.State
+import Graphics.UI.SDL as SDL
+
+import Gemstone.GL
+import Gemstone.Loop
+
+gemstoneMain :: a -> Loop a -> IO ()
+gemstoneMain globals loop = withInit [InitEverything] $ do
+    initial <- getInitialGems
+    checkExtensions
+    void $ runStateT loop (initial, globals)
