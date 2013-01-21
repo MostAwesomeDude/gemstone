9e180b79c6b35c17c2876ad46ee146e9aefe44e3
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-01-20 22:55:37 -0800

    Sprite: Refactor to permit alpha and more materials.

--- Gemstone/Sprite.hs
+++ Gemstone/Sprite.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Gemstone.Sprite where
 
 import Control.Lens
@@ -6,32 +7,44 @@ import Graphics.Rendering.OpenGL
 import Gemstone.Box
 import Gemstone.Color
 
-data Sprite v = Colored RGB (Box v)
-              | Textured TextureObject (Box v)
+data Material = Colored RGB (Maybe GLubyte)
+              | Textured TextureObject
     deriving (Show)
 
-sBox :: Simple Lens (Sprite v) (Box v)
-sBox f (Colored c b) = fmap (Colored c) (f b)
-sBox f (Textured t b) = fmap (Textured t) (f b)
+data Sprite a = Sprite { _sMaterial :: Material
+                       , _sBox :: Box a }
+    deriving (Show)
 
-drawSprite :: Sprite GLfloat -> IO ()
-drawSprite (Colored c b) = renderPrimitive Quads quad
-    where
-    rbox = remit box
-    x = b ^. rbox . bLeft
-    y = b ^. rbox . bBot
-    x' = b ^. rbox . bRight
-    y' = b ^. rbox . bTop
-    quad = do
-        color c
+makeLenses ''Sprite
+
+addAlpha :: c -> Color3 c -> Color4 c
+addAlpha a (Color3 r g b) = Color4 r g b a
+
+drawSprite :: (Ord c, VertexComponent c) => Sprite c -> IO ()
+drawSprite (Sprite material b) = case material of
+    Colored c malpha -> renderPrimitive Quads $ do
+        case malpha of
+            Just alpha -> color $ addAlpha alpha c
+            Nothing -> color c
         vertex (Vertex2 x y)
         vertex (Vertex2 x' y)
         vertex (Vertex2 x' y')
         vertex (Vertex2 x y')
-drawSprite (Textured texobj b) = do
-    enableTextures
-    renderPrimitive Quads quad
-    disableTextures
+    Textured texobj -> do
+        texture Texture2D $= Enabled
+        activeTexture $= TextureUnit 0
+        textureBinding Texture2D $= Just texobj
+        textureFunction $= Replace
+        renderPrimitive Quads $ do
+            texCoord (TexCoord2 r s)
+            vertex (Vertex2 x y)
+            texCoord (TexCoord2 r' s)
+            vertex (Vertex2 x' y)
+            texCoord (TexCoord2 r' s')
+            vertex (Vertex2 x' y')
+            texCoord (TexCoord2 r s')
+            vertex (Vertex2 x y')
+        texture Texture2D $= Disabled
     where
     rbox = remit box
     x = b ^. rbox . bLeft
@@ -42,21 +55,6 @@ drawSprite (Textured texobj b) = do
     s = 0 :: GLfloat
     r' = 1
     s' = 1
-    enableTextures = do
-        texture Texture2D $= Enabled
-        activeTexture $= TextureUnit 0
-        textureBinding Texture2D $= Just texobj
-        textureFunction $= Replace
-    disableTextures = texture Texture2D $= Disabled
-    quad = do
-        texCoord (TexCoord2 r s)
-        vertex (Vertex2 x y)
-        texCoord (TexCoord2 r' s)
-        vertex (Vertex2 x' y)
-        texCoord (TexCoord2 r' s')
-        vertex (Vertex2 x' y')
-        texCoord (TexCoord2 r s')
-        vertex (Vertex2 x y')
 
 drawSprites :: [Sprite GLfloat] -> IO ()
 drawSprites = mapM_ drawSprite
